e4abb96125bfe2c9463e7e9db91086ed371af65f
drejcar <drejcar@gmail.com>
2018-01-11 22:08:00 +0100

    work so far

---
+++ 3D_Darts/Assets/LaunchArcRenderer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class LaunchArcRenderer : MonoBehaviour
+{
+    private float velocity;
+    private float angle;
+    public int resolution = 10;
+    float g;
+    float radianAngle;
+    LineRenderer lr;
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        g = Mathf.Abs(Physics2D.gravity.y);
+    }
+    void OnValidate()
+    {
+        if (lr != null && Application.isPlaying)
+        {
+            RenderArc();
+        }
+    }
+  
+    void Start()
+    {
+        RenderArc();
+    }
+  
+    void RenderArc()
+    {
+
+        lr.SetVertexCount(resolution + 1);
+
+        lr.SetPositions(CalculateArcArray());
+    }
+
+    Vector3[] CalculateArcArray()
+    {
+        Vector3[] arcArray = new Vector3[resolution + 1];
+
+        radianAngle = Mathf.Deg2Rad * angle;
+        float maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
+        for (int i = 0; i < resolution + 1; i++)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = CalculateArcPoint(t, maxDistance);
+        }
+        return arcArray;
+    }
+    Vector3 CalculateArcPoint(float t, float maxDistance)
+    {
+        float x = t * maxDistance;
+        float y = x * Mathf.Tan(radianAngle) - ((g * x * x) / (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
+        return new Vector3(x, y);
+    }
+}
---
+++ 3D_Darts/Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class PlayerMovement: MonoBehaviour
+{
+    private float mx;
+    private float my;
+    public GameObject dartPrefab;
+    public Transform dartSpawn;
+    void Awake()
+    {
+
+    }
+  
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        mx += Input.GetAxis("Mouse X") * Time.deltaTime * 150.0f;
+        my -= Input.GetAxis("Mouse Y") * Time.deltaTime * 150.0f;
+        transform.eulerAngles = new Vector3(my,mx,0.0f);
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    void Fire()
+    {
+        
+        var dart = (GameObject)Instantiate(
+            dartPrefab,
+            dartSpawn.position,
+            dartSpawn.rotation);
+
+        // Add velocity to the bullet
+        dart.GetComponent<Rigidbody>().velocity = dart.transform.forward * 15;
+
+        // Destroy the bullet after 2 seconds
+        // Destroy(dart, 2.0f);
+    }
+}
